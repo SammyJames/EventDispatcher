c2227627773d42e3263c7f295f58f399f206a385
Pawkette <pawkette.heals@gmail.com>
2014-05-10 04:26:49 -0700

    Initial Commit

---
+++ EventDispatcher/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  EventDispatcher
+//
+//  Created by Sammy James on 5/10/14.
+//  Copyright (c) 2014 pawkett. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
